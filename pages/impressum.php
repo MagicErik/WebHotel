1f07249ad28b232d55d16abb020e6961a10d8f26
Dittrich Erik <erikdittrich11@gmail.com>
2023-11-03 16:37:02 +0100

    first version

---
+++ pages/impressum.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Impressum</title>
+    <link rel="stylesheet" href="../res/bootstrap-5.3.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../res/style.css">
+    <link rel="icon" type="image/jpg" href="../res/vector-hotel-icon-symbol-sign.jpg">
+    <script type = "text/javascript" src="../res/bootstrap-5.3.2-dist/js/bootstrap.js"></script>
+    <script type = "text/javascript" src="../res/bootstrap-5.3.2-dist/js/script.js"></script>
+
+</head>
+
+<body>
+<?php 
+  include('../scripts/menue.php');
+  ?>
+    <div class="box1 col-sm-6">
+        <h1>Impressum</h1>
+        <br>
+        <div class="box">
+            <table style="margin: 0%; padding: 0%;">
+                <tr >
+                    <th style="font-weight: 100;">
+                        <div >
+                            <h2>Verantwortlich für den Inhalt:</h2>
+                            <p1>Dittrich Erik <br>
+                                Langestrasße 123<br>
+                                0664 123 456 789<br>
+                                <a href="mailto:email@this.email.com">email@this.email.com</a>
+                            </p1>
+                    </th>
+                    <th>
+                        <img src="../res/PXL_20220623_105750104.jpg"   height="120px" style="margin-top: 20px;  float: left;"
+                            alt="Dittrich Erik">
+
+                    </th>
+                </tr>
+            </table>
+        </div>
+       
+
+
+        <br>
+        <div class="box">
+            <h2>Hotelkontakt:</h2>
+
+            <p1>Billton<br>
+                Billtonstraße 345<br>
+                0699 123 456 789<br>
+                <a href="mailto:billton@billton.com">billton@billton.com</a><br>
+                billton.com<br>
+            </p1>
+        </div>
+        <br>
+        <div class="box">
+            <h2>Firmenbuchnummer:<br></h2>
+
+            <p1>FN 123456a<br></p1>
+        </div>
+        <br>
+        <div class="box">
+            <h2>UID-Nummer:<br></h2>
+
+            <p1>ATU12345678<br></p1>
+        </div>
+        <br>
+        <div class="box">
+            <h2>Mitglied bei:<br></h2>
+
+            <p1>Wko<br>
+                Schwarzenbergplatz 4<br>
+                1030 Wien<br></p1>
+        </div>
+        <br>
+        <div class="box">
+            <h2>Berufshaftpflichtversicherung:<br></h2>
+            <p1>Österreichische Haftpflichtversicherung<br>
+                Versicherungstraße 327</p1><br>
+        </div>
+        <br>
+        <div class="box">
+            <h2>Urheberrecht und Bildnachweise:<br></h2>
+
+            <p1>Alle Bilder und Grafiken dieser Website gehören ausschlieslich Billton.<br></p1>
+        </div>
+        <br>
+        <div class="box">
+            <h2>Haftung für Links:<br></h2>
+
+            <p1>Unsere Website enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss
+                haben.<br>
+                Für
+                die Inhalte
+                der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich.<br>
+                Eine
+                permanente
+                inhaltliche Kontrolle der verlinkten Seiten ist ohne konkrete Anhaltspunkte einer
+                Rechtsverletzung nicht
+                zumutbar.<br> Bei
+                bekannt werden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.<br></p1>
+        </div>
+        <br>
+        <div class="box">
+            <h2>Streitschlichtung:<br></h2>
+
+            <p1>Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:<br>
+                <a href="https://ec.europa.eu/consumers/odr">https://ec.europa.eu/consumers/odr</a>. Unsere
+                E-Mail-Adresse finden Sie oben im Impressum. Wir sind
+                nicht
+                bereit oder
+                verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.
+            </p1>
+        </div>
+        <br>
+        <div class="box">
+            <h2>Datenschutz:<br></h2>
+
+            <p1>Informationen zur Verarbeitung personenbezogener Daten finden Sie in unserer
+                Datenschutzerklärung.</p1>
+        </div>
+        <br>
+    </div>
+    </div>
+</body>
+
+</html>
---
+++ pages/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Home</title>
+
+  <link rel="stylesheet" href="../res/bootstrap-5.3.2-dist/css/bootstrap.css">
+  <link rel="icon" type="image/jpg" href="../res/vector-hotel-icon-symbol-sign.jpg">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="../res/style.css">
+
+
+</head>
+
+<body>
+
+  <?php 
+  include('../scripts/menue.php');
+  ?>
+  <br><br><br>
+
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Launch demo modal
+  </button>
+
+  <div class="modal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModal">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.saddsadadsadsadsadad<br><br><br>dsadadsa</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
